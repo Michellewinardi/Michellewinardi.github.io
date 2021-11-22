d0698a1666a70e7262200bcf58ca0223e0e47f52
Michellewinardi <Michellewinardi@gmail.com>
2021-11-22 16:41:44 +0800

    movie8 update

--- movie8/js/tmdb.js
+++ movie8/js/tmdb.js
@@ -2,8 +2,8 @@ const key = 'e2f78fa39023ca1397fcf4e5d15e5782'; //sesuai API Key Anda
 const endpoint_url = 'https://api.themoviedb.org/3/';
 
 
-function getListMovie() {
-    fetch(endpoint_url + "movie/now_playing?api_key=" + key + "&language=en-US&page=1")
+function getListMovie(services, sectiontitle) {
+    fetch(endpoint_url + services + "?api_key=" + key + "&language=en-US&page=1")
     .then(status)
     .then(json)
     .then(function(data) {
@@ -12,16 +12,17 @@ function getListMovie() {
 
     var moviesHTML = "";
     data.results.forEach(function(movie) {
-    moviesHTML += `
+    moviesHTML =  moviesHTML + `
         <div class="col m3 s6">
-            <div class="card">
+            <div class="card moviecard">
                 <a href="./movie.html?id=${movie.id}">
                     <div class="card-image waves-effect waves-block waves-light">
                         <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" />
                     </div>
                 </a>
                 <div class="card-content text-center">
-                    <strong>${movie.title}</strong>
+                    <strong>${movie.title}</strong><br>
+                    <strong>Release Date : ${movie.release_date}</strong>
                 </div>
             </div>
         </div>
@@ -29,6 +30,8 @@ function getListMovie() {
     });
 
     document.getElementById("movie_list").innerHTML = moviesHTML;
+    document.getElementById("section_title").innerHTML = sectiontitle;
+
     })
     .catch(error);
 }
